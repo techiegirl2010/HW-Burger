4eebc9874a3fbd8a8c87e8e323c6e1762067c985
Joyce Buettner <iamtechiegirl@cox.net>
2019-07-30 14:47:13 -0700

    fixed little thing

--- server.js
+++ server.js
@@ -27,4 +27,3 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
-http://localhost:8080/burgers
